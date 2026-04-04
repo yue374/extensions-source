027b49425338a054724d5a446c0c957b560c1513
siritami <139092243+yue374@users.noreply.github.com>
2026-04-05 01:05:07 +0700

    1

--- src/vi/truyenhentai18/src/eu/kanade/tachiyomi/extension/vi/truyenhentai18/TruyenHentai18Filters.kt
+++ src/vi/truyenhentai18/src/eu/kanade/tachiyomi/extension/vi/truyenhentai18/TruyenHentai18Filters.kt
@@ -8,11 +8,10 @@ fun getFilters(): FilterList = FilterList(
     GenreFilter(),
 )
 
-class GenreFilter :
-    Filter.Select<String>(
-        "Thể loại",
-        genres.map { it.first }.toTypedArray(),
-    ) {
+class GenreFilter : Filter.Select<String>(
+    "Thể loại",
+    genres.map { it.first }.toTypedArray(),
+) {
     fun toUriPart(): String = genres[state].second
 }
 
